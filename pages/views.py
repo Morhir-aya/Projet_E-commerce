9342312a346f957c1abbbcbd9ff3756c93735cec
Aya Morhir <93487588+Morhir-aya@users.noreply.github.com>
2022-06-05 16:27:00 +0200

    added links

--- pages/views.py
+++ pages/views.py
@@ -6,3 +6,47 @@ def sign(request):
 def login(request):
     return render(request,'login.html')
 
+def home(request):
+    return render(request,'index.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def blog(request):
+    return render(request,'blog-grid.html')
+
+def faq(request):
+    return render(request,'faq.html')
+
+def dash(request):
+    return render(request,'dashboard.html')
+
+def order(request):
+    return render(request,'order.html')
+
+def address(request):
+    return render(request,'address.html')
+
+def profild(request):
+    return render(request,'profile-details.html')
+
+def Checkout(request):
+    return render(request,'Checkout.html')
+
+def cart(request):
+    return render(request,'cart.html')
+
+def confirm(request):
+    return render(request,'confirmation.html')
+
+def prodd(request):
+    return render(request,'product-single.html')
+
+def shopsb(request):
+    return render(request,'shop-sidebar.html')
+
+def purchaseconf(request):
+    return render(request,'purchase-confirmation.html')
