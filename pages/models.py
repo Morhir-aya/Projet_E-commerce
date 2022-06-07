bd8b6cfa1acb7aeae08a24b7c2f3e12afe1d43bf
Aya Morhir <93487588+Morhir-aya@users.noreply.github.com>
2022-06-07 13:38:42 +0200

    modified links

--- pages/models.py
+++ pages/models.py
@@ -6,3 +6,12 @@ class users(models.Model):
     password=models.CharField(max_length=500)
     def __str__(self):
         return self.email
+
+class inscrip(models.Model):
+    nom=models.CharField(max_length=50)
+    prenom=models.CharField(max_length=50)
+    username=models.CharField(max_length=50)
+    email=models.CharField(max_length=50)
+    password=models.CharField(max_length=500)
+    def __str__(self):
+        return self.email
