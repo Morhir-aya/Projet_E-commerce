505175717cda8b8a5ac2c0be2e27022d91ee7854
Aya Morhir <93487588+Morhir-aya@users.noreply.github.com>
2022-06-13 01:17:33 +0200

    ajouter imageField

--- products/models.py
+++ products/models.py
@@ -12,9 +12,10 @@ class product(models.Model):
 
     Title=models.CharField(max_length=50)
     Price=models.DecimalField(max_digits=6,decimal_places=5)
-    Description=models.CharField(max_length=500)
+    Description=models.TextField()
     Color=models.CharField(max_length=50,choices=SHIRT_SIZES)
     Quantite=models.DecimalField(max_digits=6,decimal_places=5)
+    product_image= models.ImageField(null=True,blank=True, upload_to='static/images/shop/products')
     Taille=models.CharField(max_length=4,choices=SHIRT_SIZES)
     Present_Panier=models.BooleanField(default=False)
     
