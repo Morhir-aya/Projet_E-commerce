25430b63369652450dece138d8ba48e3ec893a61
Aya Morhir <93487588+Morhir-aya@users.noreply.github.com>
2022-06-13 01:28:42 +0200

    modifier views

--- pages/urls.py
+++ pages/urls.py
@@ -4,20 +4,6 @@ from . import views
 urlpatterns = [
   path('',views.login,name='login'),
   path('sign/',views.sign,name='signin'),
-  path('home/',views.home,name='home'),
-  path('about/',views.about,name='about'),
-  path('contact/',views.contact,name='contact'),
-  path('blog/',views.blog,name='blog'),
-  path('faq/',views.faq,name='faq'),
-  path('dash/',views.dash,name='dash'),
-  path('order/',views.order,name='order'),
-  path('address/',views.address,name='address'),
-  path('profild/',views.profild,name='profild'),
-  path('Checkout/',views.Checkout,name='Checkout'),
-  path('cart/',views.cart,name='cart'),
-  path('confirm/',views.confirm,name='confirm'),
-  path('prodd/',views.prodd,name='prodd'),
-  path('shopsb/',views.shopsb,name='shopsb'),
-  path('purchaseconf/',views.purchaseconf,name='purchaseconf'),
+  
 
 ]
